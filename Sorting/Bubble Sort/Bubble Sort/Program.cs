b58787d0b5eb2e6b483d5a79a633c290dc0a4271
preethamg <gpreetham580@gmail.com>
2018-07-02 00:56:58 +0530

    Added bubble and selection sort
    Implemented bubble and selection sort in C#

---
+++ Sorting/Bubble Sort/Bubble Sort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the contents of array");
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+            Bubble_Sort(arr, arr.Length);
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadKey();
+        }
+
+        static int[] Bubble_Sort(int[] arr,int n)
+        {
+            for (int i = 0; i < n-1; i++)
+            {
+                for (int j = 0; j < n-i-1; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        Swap(arr, j, j + 1);
+                    }
+                }
+            }
+            return arr;
+        }
+
+        static void Swap(int [] arr,int index1,int index2)
+        {
+            int temp = arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = temp;
+        }
+    }
+}
---
+++ Sorting/Selection Sort/Selection Sort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Selection_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the contents of array");
+            var arr = Array.ConvertAll(Console.ReadLine().Split(), Int32.Parse);
+            Console.WriteLine();
+            foreach (var item in Selection_Sort(arr, arr.Length))
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+        }
+
+        static int[] Selection_Sort(int[] arr,int n)
+        {
+            for (int i = 0; i < n-1; i++)
+            {
+                int min = i;
+                for (int j = i+1; j < n; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+                Swap(arr, i, min);
+            }
+            return arr;
+        }
+
+        static void Swap(int[] arr,int ind1,int ind2)
+        {
+            int temp = arr[ind1];
+            arr[ind1] = arr[ind2];
+            arr[ind2] = temp;
+        }
+    }
+}

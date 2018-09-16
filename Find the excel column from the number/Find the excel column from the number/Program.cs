6f87506de52ce0a3193da97fcc67738c77d81cad
preethamg <gpreetham580@gmail.com>
2018-09-16 18:59:52 +0530

    Added code to convert excel column to number

--- Find the excel column from the number/Find the excel column from the number/Program.cs
+++ Find the excel column from the number/Find the excel column from the number/Program.cs
@@ -10,13 +10,38 @@ namespace Find_the_excel_column_from_the_number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter the number to be converted");
-            int num = Convert.ToInt32(Console.ReadLine());
-            ExcelColumn(num);
+            while (true)
+            {
+                Console.WriteLine("Please choose an option");
+                Console.WriteLine("1: Convert Page Number to Excel Sheet Column");
+                Console.WriteLine("2: Convert Excel Sheet Column to Page Number");
+                int option = Convert.ToInt32(Console.ReadLine());
+                if (option < 0 || option > 2)
+                {
+                    Console.WriteLine("Invalid Option Please Try Again");
+                }
+                else
+                {
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Please Enter the number to be converted");
+                            int num = Convert.ToInt32(Console.ReadLine());
+                            NumberToExcelColumn(num);
+                            break;
+                        case 2:
+                            Console.WriteLine("Please Enter Excel Sheet Number to be converted");
+                            string column = Console.ReadLine().ToUpper();
+                            ExcelColumnToNumber(column);
+                            break;
+                    }
+                }
+            }
+
             Console.ReadKey();
         }
 
-        static void ExcelColumn(int number)
+        static void NumberToExcelColumn(int number)
         {
             List<char> arr = new List<char>();
             while (number > 0)
@@ -39,5 +64,17 @@ namespace Find_the_excel_column_from_the_number
                 Console.Write(arr[i]);
             }
         }
+
+        public static void ExcelColumnToNumber(string column)
+        {
+            int num = 0;
+            int len = column.Length;
+            for (int i = 0; i < len; i++)
+            {
+                num += Convert.ToInt32((column[len-i-1] - 'A' + 1) * Math.Pow(26, i));
+            }
+            Console.WriteLine(num);
+        }
+
     }
 }

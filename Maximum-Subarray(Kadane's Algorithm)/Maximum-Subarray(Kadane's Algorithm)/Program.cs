344975c3b59b9238cb6549ecdb24c803a75fd8c0
preethamg <gpreetham580@gmail.com>
2018-09-26 22:47:23 +0530

    This program solves famous maximu sub-array/Kadane's algorithm
    solution for maximum subarray/ kadane's algorithm

---
+++ Maximum-Subarray(Kadane's Algorithm)/Maximum-Subarray(Kadane's Algorithm)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Maximum_Subarray_Kadane_s_Algorithm_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int testCaseCount = Convert.ToInt32(Console.ReadLine());
+                while (testCaseCount-- > 0)
+                {
+                    int arrSize = Convert.ToInt32(Console.ReadLine());
+                    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+                    int maxEndingHere = 0; int maxEndingSoFar = Int32.MinValue;
+
+                    for (int i = 0; i < arrSize; i++)
+                    {
+                        //add elements in array in iteration to maxEndingSoFar
+                        maxEndingHere += arr[i];
+
+                        //if we find any sum greater than the current max sum then make that max sum
+                        if (maxEndingSoFar < maxEndingHere)
+                        {
+                            maxEndingSoFar = maxEndingHere;
+                        }
+
+                        //if maxEndingSoFar is less than zero then we need to reset it back to zero
+                        if (maxEndingHere < 0)
+                        {
+                            maxEndingHere = 0;
+                        }
+
+                    }
+
+                    Console.WriteLine(maxEndingSoFar);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
